b403ae4154bc431c61aad8aa581bf856edcd8f30
Lucas Rosa <l_r_rosa@hotmail.com>
2024-05-09 21:27:59 -0300

    alteracoes no processo de request

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -1,13 +1,7 @@
 import axios from "axios";
-import { JokeResponseDto } from "../types/JokeResponseDto";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-const axiosInstance = axios.create({
+export const axiosInstance = axios.create({
   baseURL: API_URL,
 });
-
-export const getRandomJoke = async () => {
-  const response = await axiosInstance.get<JokeResponseDto>(`random-joke`);
-  return response.data;
-};
--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -1,24 +1,29 @@
 import { AxiosError } from "axios";
 import { enqueueSnackbar } from "notistack";
 import { useCallback, useState } from "react";
+import { axiosInstance } from "../api/requests";
 
-export function useRequest<Response>(request: () => Promise<Response>) {
+export function useGetRequest<Response>(url: string) {
   const [data, setData] = useState<Response>();
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState<string>();
 
   const execute = useCallback(async () => {
     setIsLoading(true);
+    const abortController = new AbortController();
     try {
-      const response = await request();
-      setData(response);
+      const response = await await axiosInstance.get<Response>(url, {
+        signal: abortController.signal,
+      });
+      setData(response.data);
     } catch (error) {
       setErrorMessage((error as AxiosError).message);
       enqueueSnackbar((error as AxiosError).message, { variant: "error" });
     } finally {
       setIsLoading(false);
     }
-  }, [request]);
+    return () => abortController.abort();
+  }, [url]);
 
   return { data, isLoading, errorMessage, execute };
 }
